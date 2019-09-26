fbf7e739ccc33c522295e9164f3f79adc568170b
Peter Styk <peter@styk.tv>
2019-09-26 23:22:02 +0100

    update pypi badge update setup

--- setup.py
+++ setup.py
@@ -3,17 +3,24 @@
 from setuptools import setup
 
 setup(
-    name = '4bars',
+    name = 'fourbars',
     version = '0.2.0',
-    description = 'In Ableton Live, automatically record 4 bar sequences and export perfect loops catalogue them.',
+    description = 'Ableton Live CLI - High Precision Loop Production and Asset Management',
     long_description = open("README.md", "r").read(),
     long_description_content_type = "text/markdown",
-    author = 'Piotr Styk',
-    author_email = 'polfilm@gmail.com',
+    author = 'Peter Styk',
+    author_email = 'dev@4bars.media',
     url = 'https://github.com/styk-tv/4bars',
-    packages = ['4bars'],
-    install_requires = ['pyliblo >= 0.9.1', 'cython'],
-    keywords = ('sound', 'music', 'ableton', 'osc','pylive'),
+    packages = ['fourbars'],
+    install_requires = [
+        'cython==0.29.13',
+        'termcolor==1.1.0',
+        'live @ git+ssh://git@github.com:ideoforms/pylive.git@cacb9ed69e129f4471d09f7a9548a5a6c5e32f13#egg=live',
+        'randomnames @ git+ssh://git@github.com:styk-tv/python-randomnames.git@beaa1fad993bf03ac5bc6f3ace2eaed119585f80#egg=randomnames',
+        'yamlordereddictloader==0.4.0',
+        'pyliblo >= 0.9.1'
+        ],
+    keywords = ['sound', 'music', 'ableton', 'osc', 'pylive'],
     classifiers = [
         'Topic :: Multimedia :: Sound/Audio',
         'Topic :: Artistic Software',
