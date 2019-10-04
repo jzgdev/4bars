d1df278a8f483a8a25de3c94a5a7238bcde5832e
Peter Styk <peter@styk.tv>
2019-10-04 02:21:37 +0100

    track notes (position, duration) as ticks working to test do: 4bars mid notes

--- fourbars/parser_track.py
+++ fourbars/parser_track.py
@@ -2,6 +2,42 @@ import mido
 import re
 import pretty_midi
 
+
+class Note(object):
+    # PYLIVE EXAMPLE
+    # note position duration velocity
+    # self.clip.add_note(60, 0, 0.25, 60)
+    # self.clip.add_note(60, 1, 0.25, 10)
+    # self.clip.add_note(60, 2, 0.25, 120)
+    # self.clip.add_note(61, 3, 1, 120)
+
+    note = None
+    channel = None
+    position = None
+    duration = None
+    velocity = None
+    is_playing = None
+    _ticks_start = None
+    _ticks_end = None
+
+    def __init__(self, in_note_on, in_clip_cursor):
+        self.is_playing = True
+        self.note = in_note_on.note
+        self.note_pretty = pretty_midi.note_number_to_name(self.note)
+        self.velocity = in_note_on.velocity
+        self.channel = in_note_on.channel
+        self._ticks_start = in_clip_cursor
+        pass
+
+    def __str__(self):
+        return 'Note(note={0} velocity={1} ticks_start={2} ticks_end={3} is_playing={4})'.format(self.note_pretty, self.velocity, self._ticks_start, self._ticks_end, self.is_playing)
+
+    def stop(self, in_clip_cursor):
+        self.is_playing = False
+        self._ticks_end = in_clip_cursor
+        # TODO: calculate position and duration
+
+
 class ParserTrack(object):
 
     mid_track = None
@@ -13,6 +49,8 @@ class ParserTrack(object):
     time_32nds_per_quarternote = 8
     time_signature = "4/4"
     track_string = ""
+    clip_notes = []
+    clip_cursor = 0
 
     def __init__(self, in_track):
         self.mid_track = in_track
@@ -20,18 +58,25 @@ class ParserTrack(object):
 
         self._iterate()
 
-        pass
+    def __str__(self):
+        return self.track_string
 
     def _iterate(self):
         print()
         for msg in self.mid_track:
+            self.clip_cursor += msg.time
             if msg.is_meta and msg.type == 'time_signature':
                 self._parse_time_signature(msg)
-            elif msg.type == 'note_on':
-                self._parse_notes(msg)
-            elif msg.type == 'note_off':
-                self._parse_notes(msg)
-
+            elif msg.type.startswith('note_on'):
+                # new note append to master index at cursor location
+                self.clip_notes.append(Note(msg, self.clip_cursor))
+            elif msg.type.startswith('note_off'):
+                # ending not currently playing
+                # find note, mark end at clip cursor
+                for c_note in self.clip_notes:
+                    if c_note.note == msg.note:
+                        c_note.stop(self.clip_cursor)
+        self.print_clip_notes()
 
     def _parse_time_signature(self, in_msg):
         self.time_is_default = False
@@ -41,12 +86,20 @@ class ParserTrack(object):
         self.time_32nds_per_quarternote = in_msg.notated_32nd_notes_per_beat
         self.time_signature = "{0}/{1}".format(self.time_numerator, self.time_denominator)
 
-    def _parse_notes(self, in_msg):
-        # translate to pylive compativle
-        # note position duration velocity
-        self.track_string += pretty_midi.note_number_to_name(in_msg.note) + ""
-        print (in_msg)
-        return
+    def print_clip_notes(self):
+        for cn in self.clip_notes:
+            self.track_string += "{0}\n".format(cn)
+        pass
+
+
+
+
+    #    # translate to pylive compativle
+    #    # note position duration velocity
+    #    for cn in self.clip_notes:
+    #    self.track_string += pretty_midi.note_number_to_name(in_msg.note) + ""
+    #    print (in_msg)
+    #    return
 
 
 
